99d2cc8ba9ae786adff42f7a3d4804703839eec4
wematu <simplyubiquitous2020@gmail.com>
2024-07-17 09:04:59 +0000

    load base url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,11 @@ import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import NotificationProvider from "@/context/NotificationContext";
 import MessageProvider from "@/context/MessageContext";
+import { useEffect } from "react";
 import setAuthTokenHeader from "@/actions/utils/setAuthToken";
 const inter = Inter({ subsets: ["latin"] });
 
-setAuthTokenHeader();
+// setAuthTokenHeader();
 
 export default function RootLayout({
   children,
@@ -23,6 +24,10 @@ export default function RootLayout({
       },
     },
   });
+
+  useEffect(() => {
+    setAuthTokenHeader();
+  }, []);
 
   return (
     <html lang="en" content="noindex, nofollow">
